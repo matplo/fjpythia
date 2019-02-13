7ccd57bbf71a692b7b63ee4bb49534f903e29529
Mateusz Ploskon <mploskon@lbl.gov>
2019-02-12 23:37:49 -0800

    cleanups

---
+++ scripts/cleanup_external_lib_builds.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function abspath()
+{
+        case "${1}" in
+                [./]*)
+                [ ! -d ${1%/*} ] && echo "${1}" && return
+                echo "$(cd ${1%/*}; pwd)/${1##*/}"
+                ;;
+                *)
+                echo "${PWD}/${1}"
+                ;;
+        esac
+}
+
+function thisdir()
+{
+        THISFILE=`abspath $BASH_SOURCE`
+        XDIR=`dirname $THISFILE`
+        if [ -L ${THISFILE} ];
+        then
+                target=`readlink $THISFILE`
+                XDIR=`dirname $target`
+        fi
+
+        THISDIR=$XDIR
+        echo $THISDIR
+}
+
+this_file_directory=`thisdir`
+
+for d in downloads
+do
+	sdir="${this_file_directory}/../${d}"
+	echo "... removing: ${sdir}"
+	rm -rf ${sdir}
+done
